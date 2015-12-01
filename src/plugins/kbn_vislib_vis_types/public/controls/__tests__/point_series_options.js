a6dbd827ad82c28243bbebc0a3800b2211ba312d
spalger <email@spalger.com>
2015-12-01 15:01:26 -0600

    give tests a vis-editor-vis-options directive

--- src/plugins/kbn_vislib_vis_types/public/controls/__tests__/point_series_options.js
+++ src/plugins/kbn_vislib_vis_types/public/controls/__tests__/point_series_options.js
@@ -1,5 +1,19 @@
+import ngMock from 'ngMock';
+import $ from 'jquery';
+
 describe('point series options', function () {
   describe('Set Y-Axis Extents', function () {
+    let compile;
+
+    beforeEach(ngMock.module('kibana'));
+    beforeEach(ngMock.inject(function ($compile, $rootScope) {
+      compile = function () {
+        const $el = $('<vis-editor-vis-options>');
+        const $scope = $rootScope.$new();
+        $compile($el)($scope);
+      };
+    }));
+
     context('min is less than max', function () {
       it('hides the error message');
     });
